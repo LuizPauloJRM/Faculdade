4dc73db6bb2854f0c464cd3f7c8565b764a26264
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-09-05 19:30:34 -0300

    - O JS não é Java. - TypeScript é um “JS melhorado” com tipagem. - O navegador executa o código JS, mas com restrições para segurança. - Funções + variáveis = base do JavaScript.

---
+++ semestre 3/AplicacoesFrontend/exercicios/ex02-EstruturaVariaveiseFuncoes/js/codigo.js
@@ -0,0 +1,12 @@
+// Declarando variáveis
+var a;
+var name = "Renan";
+
+// NaN (Not a Number)
+parseInt("hello", 10); // retorna NaN
+
+// Operadores
+var soma = 5 + 3;   // 8
+var sub = 10 - 2;   // 8
+var mult = 4 * 2;   // 8
+var div = 16 / 2;   // 8
